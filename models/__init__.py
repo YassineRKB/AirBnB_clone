dd8cd59e2a4160e402a8aa90450bd82fabc3f0e5
Yassine Rakibi <yassine@arcraven.com>
2023-08-12 05:44:49 +0200

    basemodel ==>> typo fix+ clean up

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""storage engine initiator & classes preparation module"""
+
+import models
+from models.base_model import BaseModel
+
+overviewDict = {
+    "BaseModel": BaseModel
+}
