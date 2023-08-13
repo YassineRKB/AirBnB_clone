5a020d491af7723421779881b4485c2a4b5b3424
Yassine Rakibi <yassine@arcraven.com>
2023-08-13 23:23:00 +0200

    console::update

--- console.py
+++ console.py
@@ -107,34 +107,56 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """Updates instance attributes using class name and id"""
         args = line.split()
+        
         if len(args) < 2:
             print("** class name missing **")
             return
+        
         class_name = args[0]
+        instance_id = None
+        attribute_name = None
+        new_value = None
+        
+        if len(args) >= 2:
+            instance_id = args[1]
+        if len(args) >= 3:
+            attribute_name = args[2]
+        if len(args) >= 4:
+            new_value = " ".join(args[3:]).strip('"').strip("'")
+        
+        if not class_name:
+            print("** class name missing **")
+            return
+        
         if class_name not in models.available_classes():
             print("** class doesn't exist **")
             return
-        if len(args) < 2:
+        
+        if not instance_id:
             print("** instance id missing **")
             return
-        instance_id = args[1]
+        
         key = f"{class_name}.{instance_id}"
         instance_dict = storage.all()
+        
         if key not in instance_dict:
             print("** no instance found **")
             return
+        
         instance = instance_dict[key]
-        if len(args) < 3:
+        
+        if not attribute_name:
             print("** attribute name missing **")
             return
-        attribute_name = args[2]
+        
         if not hasattr(instance, attribute_name):
             print("** attribute doesn't exist **")
             return
-        if len(args) < 4:
+        
+        if not new_value:
             print("** value missing **")
             return
-        new_value = " ".join(args[3:]).strip('"').strip("'")
+        
         setattr(
             instance,
             attribute_name,
