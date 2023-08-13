d67458f4208dc5e9730a8b2b2247b66963f75073
Yassine Rakibi <yassine@arcraven.com>
2023-08-13 02:06:52 +0200

    console: all,show,create,destroy,update

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ import re
 class HBNBCommand(cmd.Cmd):
     """Project Console: class file"""
     prompt = "(hbnb) "
+    classes = {"BaseModel"}
 
     def do_EOF(self, line):
         """Quit command to exit the program"""
@@ -20,6 +21,100 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         print()
         return True
+
+    def do_show(self, line):
+        """shows string representaion of an instance"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        class_name = args[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_name = f"{class_name}.{args[1]}"
+        if instance_name not in storage.all().keys():
+            print("** no instance found **")
+        else:
+            print(storage.all()[instance_name])
+
+    def do_create(self, line):
+        """Creates a new instance and saves it to storage"""
+        if not line:
+            print("** class name missing **")
+            return
+        class_name = line.strip()
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        instance = eval(line)()
+        instance.save()
+        print(instance.id)
+
+    def do_destroy(self, line):
+        """Destroys an instance using class name and instance id"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        class_name = args[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        instance_id = args[1]
+        instance_name = f"{class_name}.{instance_id}"
+        if instance_name not in storage.all().keys():
+            print("** no instance found **")
+        else:
+            del storage.all()[instance_name]
+            storage.save()
+
+    def do_all(self, line):
+        """Shows string representation of all instances"""
+        args = line.split()
+        objects = []
+        if not args:
+            objects = list(storage.all().values())
+        elif args[0] in HBNBCommand.classes:
+            class_name = args[0]
+            for key, obj in storage.all().items():
+                if class_name in key:
+                    objects.append(obj)
+        else:
+            print("** class doesn't exist **")
+            return
+        print(objects)
+
+    def do_update(self, line):
+        """Updates instance attributes using class name and id"""
+        args = line.split()
+        if len(args) >= 4:
+            class_name = args[0]
+            instance_id = args[1]
+            attribute_name = args[2]
+            new_value = " ".join(args[3:]).strip('"').strip("'")
+            key = f"{class_name}.{instance_id}"
+            if key not in storage.all().keys():
+                print("** no instance found **")
+                return
+            instance = storage.all()[key]
+            if not hasattr(instance, attribute_name):
+                print("** attribute doesn't exist **")
+                return
+            setattr(
+                instance,
+                attribute_name,
+                type(getattr(instance, attribute_name))(new_value)
+            )
+            storage.save()
+        else:
+            print("** incorrect number of arguments **")
 
     def emptyline(self):
         """pass on empty line"""
--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(ids())
             self.created_at = dt.now()
-            self.updtaed_at = dt.now()
+            self.updated_at = dt.now()
             models.storage.new(self)
         else:
             for key, value in kwargs.items():
@@ -23,7 +23,7 @@ class BaseModel:
                         setattr(self, key, str(value))
                     elif key == "updated_at" or "created_at":
                         dateFormat = "%Y-%m-%dT%H:%M:%S.%f"
-                        setattr(self, key, dt.strftime(value, dateFormat))
+                        setattr(self, key, dt.strptime(value, dateFormat))
                     else:
                         setattr(self, key, value)
 
@@ -37,13 +37,13 @@ class BaseModel:
 
     def save(self):
         """Save Method to save the instance"""
-        self.updtaed_at = dt.now()
+        self.updated_at = dt.now()
         models.storage.save()
 
-    def to_dic(self):
+    def to_dict(self):
         """instance attributes as dictionary"""
         the_dict = self.__dict__.copy()
-        the_dict.update({"__class__": self.__class__.__name__})
-        the_dict.update({"created_at": str(self.created_at.isoformat())})
-        the_dict.update({"updated_at": str(self.updtaed_at.isoformat())})
+        the_dict["__class__"] = type(self).__name__
+        the_dict["updated_at"] = the_dict["updated_at"].isoformat()
+        the_dict["created_at"] = the_dict["created_at"].isoformat()
         return the_dict
