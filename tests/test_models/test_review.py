07813447346657f46f7eaab27964d95f0f770733
Yassine Rakibi <yassine@arcraven.com>
2023-08-14 04:49:39 +0200

    unitests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -8,29 +8,29 @@ from models.review import Review
 class TestReview(unittest.TestCase):
     """unitest Review class"""
 
-    def review_test_module_doc(self):
+    def test_review_module_doc(self):
         """Test: existance of the module documentation"""
         self.assertIsNotNone(models.review.__doc__)
 
-    def review_test_class_doc(self):
+    def test_review_class_doc(self):
         """Test: Review class has documentation"""
         self.assertIsNotNone(Review.__doc__)
 
-    def review_test_attribute_place_id(self):
+    def test_review_attribute_place_id(self):
         """Test: place id"""
         review = Review()
         self.assertTrue(hasattr(review, "place_id"))
         self.assertEqual(review.place_id, "")
         self.assertTrue(type(review.place_id) == str)
 
-    def review_test_attribute_user_id(self):
+    def test_review_attribute_user_id(self):
         """Test: with the user ID"""
         review = Review()
         self.assertTrue(hasattr(review, "user_id"))
         self.assertEqual(review.user_id, "")
         self.assertTrue(type(review.user_id) == str)
 
-    def review_test_attribute_text(self):
+    def test_review_attribute_text(self):
         """Test: review with text"""
         review = Review()
         self.assertTrue(hasattr(review, "jeffry's confession"))
