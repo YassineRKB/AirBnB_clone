1377d6ee9a663b7d50dc0a1c98cd04091c466191
Yassine Rakibi <yassine@arcraven.com>
2023-08-14 04:43:46 +0200

    unitests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,18 +8,18 @@ import models.amenity
 class TestAmenity(unittest.TestCase):
     """ unitest Amenity class"""
 
-    def amenity_test_check_attribute(self):
+    def test_amenity_check_attribute(self):
         """Test: attribute check"""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
         self.assertEqual(amenity.name, "")
         self.assertTrue(type(amenity.name) == str)
 
-    def amenity_test_class_doc(self):
+    def test_amenity_class_doc(self):
         """Test: class has documentaion"""
         self.assertIsNotNone(Amenity.__doc__)
 
-    def amenity_test_module_doc(self):
+    def test_amenity_module_doc(self):
         """Test: module has documentation"""
         self.assertIsNotNone(models.amenity.__doc__)
 
